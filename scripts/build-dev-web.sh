7744d973f5d8498b8609eac61cf0d92280d815b7
Dragos Daian <daian.dragos@yahoo.com>
2024-04-08 14:41:59 +0200

    Update build-dev-web.sh

--- scripts/build-dev-web.sh
+++ scripts/build-dev-web.sh
@@ -1,12 +1,12 @@
 cd src/rapier2d-wrapper
-rmdir target/debug
+rmdir target/release
 export RUSTFLAGS="-C target-feature=+atomics,+bulk-memory"
-cargo +nightly build --features="single" --target=wasm32-unknown-emscripten -Z build-std=panic_abort,std
-mv target/wasm32-unknown-emscripten/debug/librapier2d_wrapper.a target/debug/librapier2d_wrapper.a
-mv target/wasm32-unknown-emscripten/debug/librapier2d_wrapper.d target/debug/librapier2d_wrapper.d
+cargo +nightly build --release --features="single" --target=wasm32-unknown-emscripten -Z build-std=panic_abort,std
+mv target/wasm32-unknown-emscripten/release/librapier2d_wrapper.a target/release/librapier2d_wrapper.a
+mv target/wasm32-unknown-emscripten/release/librapier2d_wrapper.d target/release/librapier2d_wrapper.d
 cd ../..
 
-scons arch=wasm32 platform=web target=template_debug debug_symbols=yes dev_build=yes
+scons arch=wasm32 platform=web target=template_release
 rm -rf Godot-Physics-Tests/addons/godot-rapier2d
 cp -rf bin/addons/godot-rapier2d Godot-Physics-Tests/addons/godot-rapier2d
 
